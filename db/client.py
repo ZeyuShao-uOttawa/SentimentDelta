52a60c6f85a0d8325276079e2aaa6d2ff0cedbcf
Zeyu Shao <zeyu.shao@hotmail.com>
2026-01-16 09:55:18 -0500

    small update for logging and indexes in db.client.py

--- db/client.py
+++ db/client.py
@@ -2,34 +2,41 @@ from pymongo import MongoClient, ASCENDING
 from pymongo.errors import ConnectionFailure
 from logger import get_logger
 
-logger = get_logger(__name__)
-
 class MongoDBClient:
     def __init__(self, uri: str, database_name: str):
         self.uri = uri
         self.database_name = database_name
+        self.logger = get_logger(__name__)
         self.client: MongoClient | None = None
         self.db = None
     
     def connect(self) -> None:
         """Create MongoDB client and verify connection."""
         try:
+            self.logger.info("Connecting to MongoDB")
             self.client = MongoClient(self.uri)
             self.client.admin.command('ping')
             self.db = self.client[self.database_name]
+
+            self.logger.info("Connected to MongoDB")
+
+            self._create_indexes()
             return True
         except Exception as e:
-            logger.error("Error connecting to MongoDB", e , exc_info=True)
+            self.logger.error("Error connecting to MongoDB", e , exc_info=True)
             return False
 
-    def init_indexes(self):
+    def _create_indexes(self) -> None:
+        self.db.news.create_index("url", unique=True, name="uniq_news_url")
+
         self.db.news.create_index(
             [("ticker", ASCENDING), ("date", ASCENDING)],
             name="news_ticker_date_idx"
         )
-
+        
         self.db.stock_prices.create_index(
             [("Ticker", ASCENDING), ("Datetime", ASCENDING)],
+            unique=True,
             name="stock_ticker_datetime_idx"
         )
 
@@ -39,3 +46,4 @@ class MongoDBClient:
             self.client.close()
             self.client = None
             self.db = None
+            self.logger.info("MongoDB connection closed")
